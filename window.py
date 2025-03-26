2bf1016bd423b76b228869839fc6f93578bb00f2
rafael0rueda <rrrm85@gmail.com>
2025-03-26 15:24:20 -0500

    Add the classes point and line, plus method draw line

---
+++ window.py
@@ -0,0 +1,40 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.root = Tk()
+        self.root.title("Maze Solver")
+        self.canvas = Canvas(height=self.height, width=self.width)
+        self.canvas.pack()
+        self.window_running = False
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+    
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.window_running = True
+        while self.window_running:
+            self.redraw()
+    
+    def close(self):
+        self.window_running = False
+    
+    def draw_line(self, line, fill_color):
+        line.draw(self.canvas, fill_color)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
